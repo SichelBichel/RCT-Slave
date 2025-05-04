d2ea7d68807b5f8af366297e1b2233879c7b8f75
SichelBichel <lsiegenthaler79@gmail.com>
2025-05-04 20:29:25 +0200

    UI1

--- RCT-Slave/Form1.Designer.cs
+++ RCT-Slave/Form1.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            ConsoleOutput = new RichTextBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.ReHoGaBanner3_0_Transparent;
+            pictureBox1.Location = new Point(3, 6);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(247, 102);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // ConsoleOutput
+            // 
+            ConsoleOutput.BackColor = Color.Black;
+            ConsoleOutput.ForeColor = SystemColors.Window;
+            ConsoleOutput.Location = new Point(363, 12);
+            ConsoleOutput.Name = "ConsoleOutput";
+            ConsoleOutput.Size = new Size(425, 571);
+            ConsoleOutput.TabIndex = 1;
+            ConsoleOutput.Text = "";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.BannerRCT;
+            pictureBox2.Location = new Point(256, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 88);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 595);
+            Controls.Add(pictureBox2);
+            Controls.Add(ConsoleOutput);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "RCT-Slave";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private RichTextBox ConsoleOutput;
+        private PictureBox pictureBox2;
     }
 }
